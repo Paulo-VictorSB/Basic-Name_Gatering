06afea2ad7b11bcfb5b262e3835cc537917d66d4
Paulo Victor <paulovdbarbosa@gmail.com>
2025-05-02 07:34:21 -0300

    Melhoras no visual do relatorio PDF

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -130,91 +130,115 @@ class Admin extends BaseController
             'orientation' => 'P'
         ]);
 
-        // set starting coordinates
-        $x = 50;    // horizontal
-        $y = 50;    // vertical
-        $html = "";
-
-        // logo and app name
-        $html .= '<div style="position: absolute; left: ' . $x . 'px; top: ' . $y . 'px;">';
-        $html .= '<img src="assets/images/logo_32.png">';
-        $html .= '</div>';
-        $html .= '<h2 style="position: absolute; left: ' . ($x + 50) . 'px; top: ' . ($y - 10) . 'px;">' . APP_NAME . '</h2>';
-
-        // separator
-        $y += 50;
-        $html .= '<div style="position: absolute; left: ' . $x . 'px; top: ' . $y . 'px; width: 700px; height: 1px; background-color: rgb(200,200,200);"></div>';
-
-        // report title
-        $y += 20;
-        $html .= '<h3 style="position: absolute; left: ' . $x . 'px; top: ' . $y . 'px; width: 700px; text-align: center;">REPORT DE DADOS DE ' . date('d-m-Y') . '</h4>';
-
-        // -----------------------------------------------------------
-        // table agents and totals
-        $y += 50;
-
+        $html = '
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            color: #333;
+          }
+          h2, h3 {
+            margin: 0;
+            padding: 0;
+          }
+          h2 {
+            color: #2c3e50;
+          }
+          h3 {
+            color: #34495e;
+          }
+          table {
+            font-size: 12px;
+            border-collapse: collapse;
+            width: 100%;
+            margin-bottom: 20px;
+          }
+          th {
+            background-color: #3498db;
+            color: white;
+            padding: 8px;
+            border: 1px solid #ddd;
+            text-align: left;
+          }
+          td {
+            padding: 8px;
+            border: 1px solid #ddd;
+          }
+          tr:nth-child(even) {
+            background-color: #f9f9f9;
+          }
+        </style>';
+        
+        // logo e título
+        $html .= '<div style="position: absolute; left: 50px; top: 50px;">
+                    <img src="assets/images/logo_32.png">
+                  </div>';
+        
+        $html .= '<h2 style="position: absolute; left: 100px; top: 50px;">' . APP_NAME . '</h2>';
+        
+        // separador
+        $html .= '<div style="position: absolute; left: 50px; top: 100px; width: 700px; height: 1px; background-color: #ccc;"></div>';
+        
+        // título do relatório
+        $html .= '<h3 style="position: absolute; left: 50px; top: 130px; width: 700px; text-align: center;">
+                    RELATÓRIO DE DADOS DE ' . date('d-m-Y') . '
+                  </h3>';
+        
+        // Tabela de agentes
+        $y = 180;
         $html .= '
-            <div style="position: absolute; left: ' . ($x + 90) . 'px; top: ' . $y . 'px; width: 500px;">
-                <table style="border: 1px solid black; border-collapse: collapse; width: 100%;">
-                    <thead>
-                        <tr>
-                            <th style="width: 60%; border: 1px solid black; text-align: left;">Agente</th>
-                            <th style="width: 40%; border: 1px solid black;">N.º de Clientes</th>
-                        </tr>
-                    </thead>
-                    <tbody>';
+        <div style="position: absolute; left: 140px; top: ' . $y . 'px; width: 500px;">
+          <h3 style="text-align: center;">Resumo por Agente</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>Agente</th>
+                <th>N.º de Clientes</th>
+              </tr>
+            </thead>
+            <tbody>';
+        
         foreach ($agents as $agent) {
-            $html .=
-                '<tr style="border: 1px solid black;">
-                    <td style="border: 1px solid black;">' . $agent->agente . '</td>
-                    <td style="text-align: center;">' . $agent->total_clientes . '</td>
-                </tr>';
-            $y += 25;
+            $html .= '
+              <tr>
+                <td>' . htmlspecialchars($agent->agente) . '</td>
+                <td style="text-align: center;">' . $agent->total_clientes . '</td>
+              </tr>';
         }
-
         $html .= '
             </tbody>
-            </table>
-            </div>';
-
-        // -----------------------------------------------------------
-        // table globals
-        $y += 50;
-
+          </table>
+        </div>';
+        
+        // Tabela de estatísticas globais
+        $y += (count($agents) * 30) + 100;
         $html .= '
-            <div style="position: absolute; left: ' . ($x + 90) . 'px; top: ' . $y . 'px; width: 500px;">
-                <table style="border: 1px solid black; border-collapse: collapse; width: 100%;">
-                    <thead>
-                        <tr>
-                            <th style="width: 60%; border: 1px solid black; text-align: left;">Item</th>
-                            <th style="width: 40%; border: 1px solid black;">Valor</th>
-                        </tr>
-                    </thead>
-                    <tbody>';
-
-        $html .= '<tr><td>Total agentes:</td><td style="text-align: right;">' . $global_stats['total_agents']->value . '</td></tr>';
-        $html .= '<tr><td>Total clientes:</td><td style="text-align: right;">' . $global_stats['total_clients']->value . '</td></tr>';
-        $html .= '<tr><td>Total clientes removidos:</td><td style="text-align: right;">' . $global_stats['total_deleted_clients']->value . '</td></tr>';
-        $html .= '<tr><td>Média de clientes por agente:</td><td style="text-align: right;">' . sprintf("%.2f", $global_stats['average_clients_per_agent']->value) . '</td></tr>';
-
+        <div style="position: absolute; left: 140px; top: ' . $y . 'px; width: 500px;">
+          <h3 style="text-align: center;">Estatísticas Gerais</h3>
+          <table>
+            <tbody>
+              <tr><td>Total de agentes:</td><td style="text-align: right;">' . $global_stats['total_agents']->value . '</td></tr>
+              <tr><td>Total de clientes:</td><td style="text-align: right;">' . $global_stats['total_clients']->value . '</td></tr>
+              <tr><td>Clientes removidos:</td><td style="text-align: right;">' . $global_stats['total_deleted_clients']->value . '</td></tr>
+              <tr><td>Média de clientes por agente:</td><td style="text-align: right;">' . sprintf("%.2f", $global_stats['average_clients_per_agent']->value) . '</td></tr>';
+        
         if (empty($global_stats['younger_client']->value)) {
-            $html .= '<tr><td>Idade do cliente mais novo:</td><td style="text-align: right;">-</td></tr>';
+            $html .= '<tr><td>Cliente mais novo:</td><td style="text-align: right;">-</td></tr>';
         } else {
-            $html .= '<tr><td>Idade do cliente mais novo:</td><td style="text-align: right;">' . $global_stats['younger_client']->value . ' anos.</td></tr>';
+            $html .= '<tr><td>Cliente mais novo:</td><td style="text-align: right;">' . $global_stats['younger_client']->value . ' anos</td></tr>';
         }
         if (empty($global_stats['oldest_client']->value)) {
-            $html .= '<tr><td>Idade do cliente mais velho:</td><td style="text-align: right;">-</td></tr>';
+            $html .= '<tr><td>Cliente mais velho:</td><td style="text-align: right;">-</td></tr>';
         } else {
-            $html .= '<tr><td>Idade do cliente mais velho:</td><td style="text-align: right;">' . $global_stats['oldest_client']->value . ' anos.</td></tr>';
+            $html .= '<tr><td>Cliente mais velho:</td><td style="text-align: right;">' . $global_stats['oldest_client']->value . ' anos</td></tr>';
         }
-
-        $html .= '<tr><td>Percentagem de homens:</td><td style="text-align: right;">' . $global_stats['percentage_males']->value . ' %</td></tr>';
-        $html .= '<tr><td>Percentagem de mulheres:</td><td style="text-align: right;">' . $global_stats['percentage_females']->value . ' %</td></tr>';
-
+        
         $html .= '
-                    </tbody>
-                </table>
-            </div>';
+              <tr><td>Homens (%):</td><td style="text-align: right;">' . $global_stats['percentage_males']->value . ' %</td></tr>
+              <tr><td>Mulheres (%):</td><td style="text-align: right;">' . $global_stats['percentage_females']->value . ' %</td></tr>
+            </tbody>
+          </table>
+        </div>';
+        
 
         // -----------------------------------------------------------
 
